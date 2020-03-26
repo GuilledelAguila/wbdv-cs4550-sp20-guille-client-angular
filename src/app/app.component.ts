b1fc069692765a5572d76300f65e66f972ca7a1b
Guillermo del Aguila <gdelagui@purdue.edu>
2020-03-26 19:30:24 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'wbdv-cs4550-sp20-guille-client-angular';
+}
