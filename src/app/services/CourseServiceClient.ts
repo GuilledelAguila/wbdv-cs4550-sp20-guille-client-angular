dc94e4be1d12af173fcdb5ea304f81f16c7e1160
Guillermo del Aguila <gdelagui@purdue.edu>
2020-03-27 02:15:34 +0100

    changes http to https

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -3,10 +3,10 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class CourseServiceClient {
   findAllCourses = () =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/guille/courses')
+    fetch('https://wbdv-generic-server.herokuapp.com/api/guille/courses')
       .then(response => response.json())
 
   findCourseById = (cid) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/guille/courses/${cid}`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/guille/courses/${cid}`)
       .then(response => response.json())
 }
